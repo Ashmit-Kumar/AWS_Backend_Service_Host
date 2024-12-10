51dcde89bc862d4e3651fffc6d3da2692f03a1b1
Ashmit <ashmitkumar1020@gmail.com>
2024-12-10 22:12:10 +0530

    Complete Backend

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const app = express();
+const express =require('express');
+const mongoose=require('mongoose');
+const cors=require('cors')
+const app=express();
+const dotenv=require('dotenv');
 
+// MiddleWare
 app.use(cors());
-app.use(express.json());  // To parse JSON bodies
+app.use(express.json());
+dotenv.config();
+// MongoDB Schema
+const DetailSchema=new mongoose.Schema({
+  name:String,
+  email: String,
+  message:String,
+},{timestamps:true})
 
-// Example route
-app.get('/api/data', (req, res) => {
-  res.json({ message: "Hello from the backend API" });
+const detail=mongoose.model('Detail',DetailSchema);
+
+app.post('/api/details',async(req,res)=>{
+  try{
+    const {name, email , message}=req.body;
+    const newDetail=new detail({name,email,message});
+    const saveDetail=await newDetail.save();
+    res.status(201).json(saveDetail);
+  }catch(error){
+    console.error('Error Savinf detail:', error);
+    res.status(500).json({error:'Failed to Save data'});
+  }
 });
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/')
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+app.get('/api/datails',async (req,res)=>{
+  try{
+    const details=await detail.find();
+    res.status(200).json(details);
+  }catch(error){
+    console.error("error fetching data:",error);
+    res.status(500).json({error:'Failed to Save Data'});
+  }
+})
 
-const port =  5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+mongoose.connect(`${process.env.MONGO_URI}`)
+  .then(()=> console.log("MongoDB Connected"))
+  .catch(err=> console.log(err));
+
+app.listen(5000,()=>console.log('Server running on port 5000'))
